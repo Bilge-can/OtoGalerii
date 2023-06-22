661047a6685c36e963f0b8fd8cfdf3a644f51804
Bilge-can <bilgecan618@gmail.com>
2023-06-22 18:23:05 +0300

    ilanVermeİşlemiGerçekleştirme

--- app/src/main/java/com/example/otogaleriii/IlanBilgileri.kt
+++ app/src/main/java/com/example/otogaleriii/IlanBilgileri.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.example.otogaleriii.data.model.IlanVerPojo
 
 class IlanBilgileri : AppCompatActivity() {
 
@@ -22,11 +24,20 @@ class IlanBilgileri : AppCompatActivity() {
         ilanBilgiButton = findViewById(R.id.ilanBilgiButton)
         ilanAciklamaEditText = findViewById(R.id.ilanAciklamaEditText)
         ilanBaslikEditText = findViewById(R.id.ilanBaslikEditText)
+        ilanAciklamaEditText.setText(IlanVerPojo.getAciklama())
+        ilanBaslikEditText.setText(IlanVerPojo.getBaslik())
         ilanBilgiButton.setOnClickListener {
-            val intent = Intent(this, IlanTuru::class.java)
-            startActivity(intent)
-            overridePendingTransition(R.anim.anim_in,R.anim.anim_out)
-            finish()
+            if (ilanAciklamaEditText.getText().toString() != null && ilanBaslikEditText.getText().toString() != null) {
+                IlanVerPojo.setAciklama(ilanAciklamaEditText.getText().toString())
+                IlanVerPojo.setBaslik(ilanBaslikEditText.getText().toString())
+                val intent = Intent(this, IlanTuru::class.java)
+                startActivity(intent)
+                overridePendingTransition(R.anim.anim_in, R.anim.anim_out)
+                finish()
+            } else{
+                Toast.makeText(getApplicationContext(),"Lütfen tüm bilgileri giriniz!",Toast.LENGTH_LONG).show()
+            }
+
         }
         ilanBilgiButtonGeri = findViewById(R.id.ilanBilgiButtonGeri)
         ilanBilgiButtonGeri.setOnClickListener {
