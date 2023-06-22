dad4cf5169dc1a399a687974083e14a20204f681
Bilge-can <bilgecan618@gmail.com>
2023-06-22 14:52:02 +0300

    ilanVermeEkranTasarım

---
+++ app/src/main/java/com/example/otogaleriii/MotorPerformans.kt
@@ -0,0 +1,11 @@
+package com.example.otogaleriii
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MotorPerformans : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_motor_performans)
+    }
+}
